59075dc2eeb56c86cf174c595126c3d464844f2f
Adriani6 <adrian@adriani6.co.uk>
2015-11-20 01:14:48 +0000

    Version 1.0 Finished.
    Signed-off-by: Adriani6 <adrian@adriani6.co.uk>

--- popup.js
+++ popup.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
+function checkNotifications(){
+	
 	$('#result').load('http://www.spigotmc.org #AlertsMenu_Counter', function(){
-		alert($("#result").text());
+	$('#messages').load('http://www.spigotmc.org #ConversationsMenu_Counter');
+	var notifications = $("#result").text().replace(/\s/g, "");
+	var messages = $("messages").text().replace(/\s/g, "");
+	
+	var total = Number(notifications) + Number(messages);
+	
+		chrome.browserAction.setBadgeText({text: total.toString()});
+
 	});
+}
+	
+$(document).ready(function(){
+	checkNotifications();
+	setInterval(function(){ 
+		checkNotifications();
+	}, 30000);
 });
