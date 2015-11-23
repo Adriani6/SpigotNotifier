4a65222887f0da189072b9c102a2706458f692c2
Adriani6 <adrian@adriani6.co.uk>
2015-11-23 00:37:42 +0000

    Version 1.1: Fixed hyperlink, now opens new tab with Spigot Forums, Fixed messages which were not displayed in tray, Added Notifications pop-ups.
    Signed-off-by: Adriani6 <adrian@adriani6.co.uk>

--- popup.js
+++ popup.js
@@ -1,14 +1,23 @@
+var id = 'SpigotNotifier';
+var oldData = [0, 0];
+var messages = 0;
+var notifications = 0;
+
 function checkNotifications(){
 	
 	$('#result').load('http://www.spigotmc.org #AlertsMenu_Counter', function(){
-	$('#messages').load('http://www.spigotmc.org #ConversationsMenu_Counter');
-	var notifications = $("#result").text().replace(/\s/g, "");
-	var messages = $("messages").text().replace(/\s/g, "");
-	
-	var total = Number(notifications) + Number(messages);
-	
-		chrome.browserAction.setBadgeText({text: total.toString()});
+		$('#messages').load('http://www.spigotmc.org #ConversationsMenu_Counter', function(){
+			
+			oldData[0] = notifications;
+			oldData[1] = messages;
+		
+			notifications = $("#result").text().replace(/\s/g, "");
+			messages = $("#messages").text().replace(/\s/g, "");
 
+			var total = parseInt(notifications) + parseInt(messages);
+	
+			chrome.browserAction.setBadgeText({text: total.toString()});
+		});
 	});
 }
 	
@@ -16,5 +25,30 @@ $(document).ready(function(){
 	checkNotifications();
 	setInterval(function(){ 
 		checkNotifications();
-	}, 30000);
+		var notification;
+		if(notifications > oldData[0]){
+			notification = new Notification('New Notification(s)', {
+			icon: 'spigot.png',
+			body: "You've got " + notifications + " new notifications.",
+			});
+		}
+		
+		if(messages > oldData[1]){
+			notification = new Notification('New Message(s)', {
+			icon: 'spigot.png',
+			body: "You've got " + messages + " unread messages.",
+			});
+		}
+		
+	notification.onclick = function () {
+		window.open("http://www.spigotmc.org");      
+    };
+	
+	}, 15000);
+	
+	$('body').on('click', 'a', function(){
+     chrome.tabs.create({url: $(this).attr('href')});
+     return false;
+   });
+	
 });
